411778fb41308f1514d12c919e00168682e89ea3
Achraf <achraf.zibouhee@gmail.com>
2025-07-09 18:59:50 +0200

    change image Login

--- src/Components/Contact-Us/ContactUsBox/ContactUsBox.jsx
+++ src/Components/Contact-Us/ContactUsBox/ContactUsBox.jsx
@@ -60,7 +60,7 @@ const ContactUsBox = () => {
                 <div>
                   <iframe
                     className="w-full h-[26rem]"
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d732.8789914452929!2d3.4764031276840037!3d36.75039467556591!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x128e6894f50955a3%3A0xd2fb5879965cb157!2sINFORMICA%2C%20Ecole%20de%20langues%20et%20informatique!5e0!3m2!1sfr!2sdz!4v1679411659435!5m2!1sfr!2sdz"
+                    src=""
                     allowFullScreen=""
                     loading="lazy"
                     referrerPolicy="no-referrer-when-downgrade"
