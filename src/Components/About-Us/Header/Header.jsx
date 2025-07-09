d08a9d49835fb8f21bae198c57da3168ae3fda03
Achraf <achraf.zibouhee@gmail.com>
2025-07-09 19:19:42 +0200

    change image path

--- src/Components/About-Us/Header/Header.jsx
+++ src/Components/About-Us/Header/Header.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Title } from '../../../GeneralFunctions/title';
 import { useTranslation } from 'react-i18next';
-
+import AboutUs2 from './../../../../src/Assets/AboutUs2.jpg';
 const Header = () => {
   Title('Taalam | About-Us');
   const { t } = useTranslation();
 
   return (
     <div>
-      <div className="bg-[url('./../../../../src/Assets/AboutUs2.jpg')] bg-cover bg-fixed h-[70vh] relative">
+      <div
+        className="bg-cover bg-fixed h-[70vh] relative"
+        style={{ backgroundImage: `url(${AboutUs2})` }}
+      >
         <div className="container">
           <div className="w-full lg:w-12/12 text-center text-main-color absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
             <div className="mb-4 font-bold relative before:absolute before:hidden before:xl:block before:h-1 before:w-16 before:left-1/2 before:-translate-x-1/2  before:bg-second-color before:top-[115%]">
