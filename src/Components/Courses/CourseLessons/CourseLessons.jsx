95207a1f37e640b2ea0ac77457cd2648e1d9ef45
Achraf <achraf.zibouhee@gmail.com>
2025-07-09 20:41:03 +0200

    change Course Details

--- src/Components/Courses/CourseLessons/CourseLessons.jsx
+++ src/Components/Courses/CourseLessons/CourseLessons.jsx
@@ -5,28 +5,7 @@ import LessonData from './../data/Lessons.json';
 const CourseLessons = () => {
   const { courseId } = useParams();
   const lessons = LessonData.filter((lesson) => lesson.courseId == courseId);
-  return (
-    <div>
-      <h2>Lessons</h2>
-      <ul>
-        {lessons.map((lesson) => (
-          <li key={lesson.id}>
-            <h3>{lesson.title}</h3>
-            <p>{lesson.description}</p>
-            <ul>
-              {lesson.resources.map((resource) => (
-                <li key={resource.id}>
-                  <a href={resource.url} target="_blank">
-                    {resource.type}: {resource.filename}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <div>hello word</div>;
 };
 
 export default CourseLessons;
